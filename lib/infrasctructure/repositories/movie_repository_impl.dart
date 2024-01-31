ce2bd5e6300c8f9dcd4f3fe6dd90bc004c04f756
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-01-31 00:07:24 -0500

    feature()[]: add respository initial implementation

---
+++ lib/infrasctructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,18 @@
+
+
+
+import 'package:cinemafinder/domain/datasources/movies_datasource.dart';
+import 'package:cinemafinder/domain/entities/movie.dart';
+import 'package:cinemafinder/domain/repositories/movie_repository.dart';
+
+class MoviesRepositoryImpl extends MoviesRepository {
+  final MoviesDatasource datasource;
+
+  MoviesRepositoryImpl({required this.datasource});
+  
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    return datasource.getNowPlaying(page: page);
+  }
+
+}
