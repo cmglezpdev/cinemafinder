cb939958c2ae9db3411299d10341ea0867684556
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-01-31 00:05:00 -0500

    feature()[]: add class to enum env vars

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -2,5 +2,5 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
-  static String movieDBKey = dotenv.env['THE_MOVIEDB_API_KEY'] ?? 'The MovieDB Key Not Found';
+  static String theMovieDbKey = dotenv.env['THE_MOVIEDB_API_KEY'] ?? 'The MovieDB Key Not Found';
 }
