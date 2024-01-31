9aede8104927d11710d90da7fd817ce33cfac775
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-01-31 00:08:42 -0500

    feature()[]: show basic list of now playing movies

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,7 +1,7 @@
-
-
-
+import 'package:cinemafinder/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 
 class HomeScreen extends StatelessWidget {
   static const String name = 'home_screen';
@@ -11,7 +11,45 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Placeholder()
+      body: Center(
+        child: _HomeView(),
+      )
+    );
+  }
+}
+
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+  final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    if(nowPlayingMovies.isEmpty) {
+      return const CircularProgressIndicator();
+    }
+
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(nowPlayingMovies[index].title),
+          subtitle: Text(nowPlayingMovies[index].overview),
+        );
+      }
     );
   }
 }
