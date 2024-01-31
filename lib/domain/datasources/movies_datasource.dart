4f9a96fd5bdb9537bfdf92ebf7c3002f6286e6d4
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-01-31 00:04:15 -0500

    feature()[]: add datasources, entites and repositories abstractions

---
+++ lib/domain/datasources/movies_datasource.dart
@@ -0,0 +1,9 @@
+
+
+
+import 'package:cinemafinder/domain/entities/movie.dart';
+
+abstract class MoviesDatasource {
+
+  Future<List<Movie>> getNowPlaying({ int page = 1 });
+}
---
+++ lib/domain/repositories/movie_repository.dart
@@ -0,0 +1,7 @@
+
+import 'package:cinemafinder/domain/entities/movie.dart';
+
+abstract class MoviesRepository {
+
+  Future<List<Movie>> getNowPlaying({ int page = 1 });
+}
