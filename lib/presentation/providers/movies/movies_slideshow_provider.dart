bde492df9b4cf487751f13028a6131654c413afe
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-02-01 11:43:33 -0500

    fix(provider)[]: remove limit un now_playing_movies list

--- lib/presentation/providers/movies/movies_slideshow_provider.dart
+++ lib/presentation/providers/movies/movies_slideshow_provider.dart
@@ -7,5 +7,5 @@ final moviesSlideshowProvider = Provider<List<Movie>>((ref) {
   final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
 
   if(nowPlayingMovies.isEmpty) return [];
-  return nowPlayingMovies.sublist(0, 6);
+  return nowPlayingMovies;
 });
