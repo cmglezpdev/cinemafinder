2c72bf3734c39c329b72b390da6f18097511d7ce
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-02-01 09:39:36 -0500

    feature()[]: add slideshow widget + movie slideshow provider

---
+++ lib/presentation/providers/movies/movies_slideshow_provider.dart
@@ -0,0 +1,11 @@
+import 'package:cinemafinder/domain/entities/movie.dart';
+import 'package:cinemafinder/presentation/providers/movies/movies_providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+final moviesSlideshowProvider = Provider<List<Movie>>((ref) {
+  final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+  if(nowPlayingMovies.isEmpty) return [];
+  return nowPlayingMovies.sublist(0, 6);
+});
